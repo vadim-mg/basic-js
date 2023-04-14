60e337db60904de2b4d350504adb54a0bf764360
vadim-mg <vadikm1981@yandex.ru>
2023-04-14 13:22:46 +0500

    feat: transform-array

--- src/transform-array.js
+++ src/transform-array.js
@@ -1,4 +1,4 @@
-const { NotImplementedError } = require('../extensions/index.js');
+const { NotImplementedError } = require("../extensions/index.js")
 
 /**
  * Create transformed array based on the control sequences that original
@@ -12,12 +12,47 @@ const { NotImplementedError } = require('../extensions/index.js');
  * transform([1, 2, 3, '--double-next', 4, 5]) => [1, 2, 3, 4, 4, 5]
  * transform([1, 2, 3, '--discard-prev', 4, 5]) => [1, 2, 4, 5]
  * 
- */
-function transform(/* arr */) {
-  throw new NotImplementedError('Not implemented');
-  // remove line with error and write your code here
+//  */
+
+function transform(arr) {
+
+  if (!Array.isArray(arr)) {
+    throw new Error("'arr' parameter must be an instance of the Array!")
+  }
+  const result = []
+  for (let i = 0; i < arr.length; i++) {
+    let val = false
+    switch (arr[i]) {
+      case "--discard-next":
+        if (i < arr.length - 1) {
+          i = i + 1
+        }
+        break
+      case "--discard-prev":
+        if (i > 0 && arr[i - 2] !== '--discard-next') {
+          result.pop()
+        }
+        break
+      case "--double-next":
+        if (i < arr.length - 1) {
+          val = arr[i + 1]
+        }
+        break
+      case "--double-prev":
+        if (i > 0 && arr[i - 2] !== '--discard-next') {
+          val = arr[i - 1]
+        }
+        break
+      default:
+        val = arr[i]
+    }
+    if (val) {
+      result.push(val)
+    }
+  }
+  return result
 }
 
 module.exports = {
-  transform
-};
+  transform,
+}
